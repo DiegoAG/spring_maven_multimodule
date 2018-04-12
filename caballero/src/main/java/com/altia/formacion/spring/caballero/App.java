25fcf599ccd7f9093496988a7f7d379cc3684edb
javierlete <jglete@gmail.com>
2018-04-12 17:06:14 +0200

    Proyecto Maven Vacío

---
+++ caballero/src/main/java/com/altia/formacion/spring/caballero/App.java
@@ -0,0 +1,13 @@
+package com.altia.formacion.spring.caballero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
