077dd0aba56f6024e8675364c723b6abd7b33407
javierlete <jglete@gmail.com>
2018-04-16 17:20:19 +0200

    Testeo de la aplicación con autowire y SpringJUnit4ClassRunner

--- sistemasonido/src/test/java/com/altia/formacion/spring/sistemasonido/AppTest.java
+++ sistemasonido/src/test/java/com/altia/formacion/spring/sistemasonido/AppTest.java
@@ -1,38 +1,22 @@
 package com.altia.formacion.spring.sistemasonido;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertNotNull;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=CDPlayerConfig.class)
+public class AppTest {
+	
+	@Autowired
+	private CompactDisc cd;
+	
+	@Test
+	public void cdShouldNotBeNull() {
+		assertNotNull(cd);
+	}
 }
