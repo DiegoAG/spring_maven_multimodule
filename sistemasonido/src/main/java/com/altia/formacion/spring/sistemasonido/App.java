abee364fa259b2b6442c2d466577e77a71caf09d
javierlete <jglete@gmail.com>
2018-04-16 17:03:08 +0200

    Autoconfiguración a través de beans.xml

--- sistemasonido/src/main/java/com/altia/formacion/spring/sistemasonido/App.java
+++ sistemasonido/src/main/java/com/altia/formacion/spring/sistemasonido/App.java
@@ -1,15 +1,20 @@
 package com.altia.formacion.spring.sistemasonido;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	AnnotationConfigApplicationContext context =
-        	new AnnotationConfigApplicationContext(CDPlayerConfig.class);
-    	
-    	CompactDisc cd = context.getBean(SgtPeppers.class);
-    	cd.play();
-    }
+public class App {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(CDPlayerConfig.class);
+
+		CompactDisc cd = context.getBean(SgtPeppers.class);
+		cd.play();
+
+		ClassPathXmlApplicationContext contextXml = 
+				new ClassPathXmlApplicationContext("META-INF/beans.xml");
+
+		CompactDisc cd2 = contextXml.getBean(SgtPeppers.class);
+		cd2.play();
+	}
 }
