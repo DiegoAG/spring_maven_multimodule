edd3d07269fc9fc261db77f12d21b95fc145ca5a
javierlete <jglete@gmail.com>
2018-04-17 18:10:20 +0200

    Uso de propiedades en beans.xml

--- sistemasonido/src/main/java/com/altia/formacion/spring/sistemasonido/BlankDisc.java
+++ sistemasonido/src/main/java/com/altia/formacion/spring/sistemasonido/BlankDisc.java
@@ -8,10 +8,38 @@ public class BlankDisc implements CompactDisc {
 	private String artist;
 	private List<String> tracks;
 	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public List<String> getTracks() {
+		return tracks;
+	}
+
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+	
 	public BlankDisc(String title, String artist, List<String> tracks) {
 		this.title = title;
 		this.artist = artist;
 		this.tracks = tracks;
+	}
+	
+	public BlankDisc() {
+		
 	}
 	
 	public void play() {
